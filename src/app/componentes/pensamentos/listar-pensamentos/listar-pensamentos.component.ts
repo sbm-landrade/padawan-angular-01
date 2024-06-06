8e069300d259a72a30d17f03d542e9984d4e86e0
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-06 15:13:25 -0300

    Comunicação entre componentes

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -8,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class ListarPensamentosComponent implements OnInit {
 
   listaPensamentos= [
-
+  {
+    conteudo: 'Passo informações para o componente filho',
+    autoria: 'Componente pai',
+    modelo: 'modelo3'
+  },
+  {
+    conteudo: 'Minha propriedade é decorada com @Input',
+    autoria: 'Componente filho',
+    modelo: 'modelo2'
+  }
   ];
 
   constructor() { }
