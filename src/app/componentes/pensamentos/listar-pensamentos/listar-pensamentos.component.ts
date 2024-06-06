c92dc63b7fc1c16e6d474d87073f4b4ea99f8a74
Lucas Andrade <lucas.andrade@sbmtechnology.com.br>
2024-06-06 14:48:44 -0300

    Listando com "*ngFor"

--- src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/componentes/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
+  listaPensamentos= [
+
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
